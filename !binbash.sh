fcf243d8ad4a7bd047bf2ee9f68096560105e880
Leyla Gozalova <leyla.gozelovaa@gmail.com>
2024-11-29 00:30:05 +0400

    add bash script

---
+++ !binbash.sh
@@ -0,0 +1,32 @@
+!/bin/bash
+FILES=()
+EXTENSION=""
+REPLACEMENT=""
+
+while [[ $# -gt 0 ]]; do
+        case $1 in
+                --file)
+                        Files+=($2)
+                        shift
+                        shift
+                        ;;
+                        --extension)
+                                EXTENSION=$2
+                                shift
+                                shift
+                                ;;
+                        *)
+                                echo "Error argument $2 not supported"
+                                exit 1
+                esac
+        done
+        for file in "${FILES[@]}"; do
+                dirname=$(dirname $file)
+                filename=$(basename $file)
+                basename="$dirname/$filename"
+                if [[ $filename =~ \.$EXTENSION$ ]]; then
+                        new_name="${basename%.$EXTENSION}.$REPLACEMENT"
+                        mv $basename $new_name
+                        echo $new_name
+                fi
+        done
